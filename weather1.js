fab3646cc9d30fdd117c29d9112a6c9fcd319473
Varun Rao <36993436+me-codes@users.noreply.github.com>
2020-06-15 23:57:48 +0530

    Update weather1.js

--- weather1.js
+++ weather1.js
@@ -5,7 +5,7 @@ var temp = document.querySelector('.temp')
 var summary = document.querySelector('.summary')
 var iconw = document.querySelector('.icon5')
 var humidity1=document.querySelector('.humi')
-var tz1=document.querySelector('.time1')
+var wind1=document.querySelector('.wind11')
 button.addEventListener('click',function (name) {
     fetch('http://api.openweathermap.org/data/2.5/weather?q='+cityname.value+'&units=metric&APPID=4518fda2354200ccbb21eb2e9b2bb395')
 .then(response => response.json())
@@ -15,13 +15,13 @@ button.addEventListener('click',function (name) {
     var descValue = data['weather'][0]['description'];
     var iconweather = data['weather'][0]['icon'];
     var humidi =data['main']['humidity'];
-    var tz =data['timezone'];
+    var wind =data['wind']['speed'];
     city.innerHTML = "<HR color='white'>"+nameValue+"<HR color='white'>";
     summary.innerHTML = "Summary : "+descValue;
     temp.innerHTML = "Temperature : "+tempValue+"&deg C";
     iconw.innerHTML = "<img width='100pt' height='100pt' src='http://openweathermap.org/img/w/" +iconweather+ ".png' alt='Icon depicting current weather.'>"
-    humidity1.innerHTML="humidity : "+humidi+"%";
-    tz1.innerHTML = "TimeZone : "+tz;
+    humidity1.innerHTML="Humidity : "+humidi+"%";
+    wind1.innerHTML = "Wind speed : "+wind+"m/s";
     cityname.value ="";
 })
 .catch(err =>alert("Wrong city name!"))
